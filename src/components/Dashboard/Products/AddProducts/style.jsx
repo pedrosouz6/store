8fb723dfe53829df6d06a895d06636f2735f3cf9
Pedro Souza <peedrosouz6@gmail.com>
2022-04-22 18:24:16 -0300

    refactor code

--- src/components/Dashboard/Products/AddProducts/style.jsx
+++ src/components/Dashboard/Products/AddProducts/style.jsx
@@ -6,7 +6,6 @@ const {
     background_primary,
     transition,
     selected,
-    background_thirdy,
     textColor,
     errorColor
 } = Colors.light;
@@ -90,68 +89,6 @@ export const Container = styled.div `
                         background-color: ${selected};
                     }
                 }
-            }
-        }
-    }
-
-    .dashboard--products__preview__product {
-        flex: 1 1 270px;
-        max-width: 270px;
-        height: auto;
-
-        h3 {
-            margin-bottom: 1rem;
-            font-size: 16pt;
-        }
-
-        .dashboard--products__card--preview {
-            width: 100%;
-            margin: 0 auto;
-            background-color: rgb(240, 240, 240);
-        }
-
-        .dashboard--products__card__img {
-            width: 100%;
-            height: 235px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: rgb(205, 205, 205);
-            margin-bottom: .5rem;
-
-            img {
-                width: 100%;
-                height: 100%;
-            }
-        }
-
-        .dashboard--products__card__content {
-            padding: 0 10px;
-        }
-
-        .dashboard--products__card__brand__product {
-            font-size: 11pt;
-            margin-bottom: .2rem;
-        }
-
-        .dashboard--products__card__price__product {
-            margin: .8rem 0;
-            font-size: 13pt;
-        }
-
-        .dashboard--products__card__btn {
-            border-top: 1px solid ${background_thirdy};
-            padding: .5rem 0;
-            display: flex;
-            justify-content: end;
-
-            button {
-                border: none;
-                background-color: ${background_primary};
-                color: white;
-                padding: 6px 15px;
-                border-radius: 3px;
-                cursor: pointer;
             }
         }
     }
